c098b3f864b4622378cc3cd56ecf16ccabe8cb79
Mikhail Khvashchevskiy <khvashchevskiy.mikhail@gmail.com>
2021-12-16 13:02:30 +0300

    Добавил CustomAuthenticateProvider . Ихменений в работе нет.

---
+++ src/main/java/com/medclinic/config/security/CustomAuthenticateProvider.java
@@ -0,0 +1,37 @@
+package com.medclinic.config.security;
+
+import com.medclinic.entity.User;
+import com.medclinic.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticateProvider implements AuthenticationProvider {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String login = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userService.findByLogin(login);
+
+        if (user != null || user.getPassword().equals(password)){
+            return new UsernamePasswordAuthenticationToken(user.getLogin(), user.getPassword(), user.getAuthorities());
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
--- src/main/java/com/medclinic/config/security/WebSecurityConfig.java
+++ src/main/java/com/medclinic/config/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private CustomAuthenticateProvider customAuthenticateProvider;
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
@@ -34,6 +38,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setUserDetailsService(userService);
         daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
         return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(customAuthenticateProvider);
     }
 
     @Override
