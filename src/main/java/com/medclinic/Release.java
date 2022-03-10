4aff1d1487e1ec6c730e51a88f21dd002b5f68cc
Mikhail Khvashchevskiy <khvashchevskiy.mikhail@gmail.com>
2022-03-10 14:31:38 +0300

    nnn

--- src/main/java/com/medclinic/Release.java
+++ src/main/java/com/medclinic/Release.java
@@ -1,18 +1,11 @@
 package com.medclinic;
 
-import com.medclinic.dto.DescribeAnalysisDto;
-import com.medclinic.entity.*;
-import com.medclinic.repository.impl.*;
-import com.medclinic.service.impl.AnalysisService;
-import com.medclinic.service.impl.WorkFlowService;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import javax.persistence.EntityManager;
-import java.time.LocalDate;
-import java.util.*;
+/**
+ *
+ * Author: Mikhail Khvashchevskiy
+ *
+ */
 
 public class Release {
 
